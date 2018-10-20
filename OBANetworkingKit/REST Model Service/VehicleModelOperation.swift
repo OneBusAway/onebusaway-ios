7371e57a2030346a1b770289624fead565373b05
Aaron Brethorst <aaron@brethorsting.com>
2018-10-19 23:49:51 -0700

    WIP

--- OBANetworkingKit/REST Model Service/VehicleModelOperation.swift
+++ OBANetworkingKit/REST Model Service/VehicleModelOperation.swift
@@ -7,15 +7,60 @@
 //
 
 import Foundation
+import CoreLocation
 
-public class Vehicle: NSObject, Codable {
+public class VehicleStatus: NSObject, Decodable {
 
+    /// The id of the vehicle
+    let vehicleID: String
+
+    /// The last known real-time update from the transit vehicle
+    let lastUpdateTime: Date?
+
+    /// The last known real-time update from the transit vehicle containing a location update
+    let lastLocationUpdateTime: Date?
+
+    /// The last known location of the vehicle
+    let location: CLLocation?
+
+    /// The id of the vehicle's current trip, which can be used to look up the referenced `trip` element in the `references` section of the data.
+    let tripID: String
+
+    private enum CodingKeys: String, CodingKey {
+        case vehicleID = "vehicleId"
+        case lastUpdateTime = "lastUpdateTime"
+        case lastLocationUpdateTime = "lastLocationUpdateTime"
+        case location = "location"
+        case tripID = "tripId"
+    }
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        vehicleID = try container.decode(String.self, forKey: .vehicleID)
+        lastUpdateTime = try container.decode(Date.self, forKey: .lastUpdateTime)
+        lastLocationUpdateTime = try container.decode(Date.self, forKey: .lastLocationUpdateTime)
+
+        let locationModel = try container.decode(LocationModel.self, forKey: .location)
+        location = CLLocation(latitude: locationModel.latitude, longitude: locationModel.longitude)
+
+        tripID = try container.decode(String.self, forKey: .tripID)
+    }
+}
+
+private struct LocationModel: Codable {
+    let latitude: Double
+    let longitude: Double
+
+    enum CodingKeys: String, CodingKey {
+        case latitude = "lat"
+        case longitude = "lon"
+    }
 }
 
 @objc(OBAVehicleModelOperation)
 public class VehicleModelOperation: Operation {
     public var apiOperation: RESTAPIOperation?
-    public private(set) var vehicles: [Vehicle] = []
+    public private(set) var vehicles: [VehicleStatus] = []
 
     override public func main() {
         guard
@@ -26,9 +71,9 @@ public class VehicleModelOperation: Operation {
         }
 
         let decoder = DictionaryDecoder()
-        self.vehicles = entries.compactMap { vehicleDict -> Vehicle? in
+        self.vehicles = entries.compactMap { vehicleDict -> VehicleStatus? in
             do {
-                return try decoder.decode(Vehicle.self, from: vehicleDict)
+                return try decoder.decode(VehicleStatus.self, from: vehicleDict)
             }
             catch {
                 print("Unable to decode vehicle from data: \(error)")
