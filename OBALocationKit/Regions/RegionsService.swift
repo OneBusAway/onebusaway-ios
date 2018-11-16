ebc8c7f33d8a31adfb43a20b4f7d08d7409abef6
Aaron Brethorst <aaron@brethorsting.com>
2018-11-15 17:31:05 -0800

    Renames RegionsManager to RegionsService to better match other framework classes

---
+++ OBALocationKit/Regions/RegionsService.swift
@@ -0,0 +1,84 @@
+//
+//  RegionsService.swift
+//  OBALocationKit
+//
+//  Created by Aaron Brethorst on 11/10/18.
+//  Copyright © 2018 OneBusAway. All rights reserved.
+//
+
+import Foundation
+import OBANetworkingKit
+import CoreLocation
+
+public class RegionsService: NSObject {
+    private let apiService: RegionsAPIService
+    private let locationService: LocationService
+
+    public init(apiService: RegionsAPIService, locationService: LocationService) {
+        self.apiService = apiService
+        self.locationService = locationService
+
+        regions = RegionsService.loadStoredRegions()
+
+        super.init()
+
+        self.locationService.addDelegate(self)
+    }
+
+    // MARK: - Regions Data
+
+    public private(set) var regions: [Region]
+
+    public private(set) var currentRegion: Region? {
+        didSet {
+            //
+        }
+    }
+}
+
+// MARK: - Regions Data Storage
+extension RegionsService {
+    private class func loadStoredRegions() -> [Region] {
+        return []
+    }
+}
+
+// MARK: - Region Updates
+extension RegionsService: LocationServiceDelegate {
+    public func locationService(_ service: LocationService, locationChanged location: CLLocation) {
+        updateCurrentRegion(location: location)
+    }
+
+    private func updateCurrentRegion(location: CLLocation) {
+
+    }
+
+//    - (NSArray<OBARegionV2*>*)regionsWithin100Miles {
+//    if (self.regions.count == 0) {
+//    return @[];
+//    }
+//
+//    CLLocation *currentLocation = self.locationManager.currentLocation;
+//
+//    if (!currentLocation) {
+//    return @[];
+//    }
+//
+//    return [[self.regions sortedArrayUsingComparator:^NSComparisonResult(OBARegionV2 *r1, OBARegionV2 *r2) {
+//    CLLocationDistance distance1 = [r1 distanceFromLocation:currentLocation];
+//    CLLocationDistance distance2 = [r2 distanceFromLocation:currentLocation];
+//
+//    if (distance1 > distance2) {
+//    return NSOrderedDescending;
+//    }
+//    else if (distance1 < distance2) {
+//    return NSOrderedAscending;
+//    }
+//    else {
+//    return NSOrderedSame;
+//    }
+//    }] filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(OBARegionV2 *r, NSDictionary<NSString *,id> *bindings) {
+//    return ([r distanceFromLocation:currentLocation] < 160934); // == 100 miles
+//    }]];
+//    }
+}
