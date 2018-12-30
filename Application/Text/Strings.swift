94166df20b07348049fa81c41f26fe159cc266ad
Aaron Brethorst <aaron@brethorsting.com>
2018-12-30 14:21:52 -0800

    Adds a class for generic, reusable strings

---
+++ Application/Text/Strings.swift
@@ -0,0 +1,15 @@
+//
+//  Strings.swift
+//  OBAKit
+//
+//  Created by Aaron Brethorst on 12/30/18.
+//  Copyright © 2018 OneBusAway. All rights reserved.
+//
+
+import Foundation
+
+public class Strings: NSObject {
+
+    public static let close = NSLocalizedString("common.close", value: "Close", comment: "The verb 'to close'.")
+
+}
