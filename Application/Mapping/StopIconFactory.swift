c4c11fcf6c48afd06387d2676cafd54718ea8a6e
Aaron Brethorst <aaron@brethorsting.com>
2019-09-15 08:10:00 -0700

    Documents and cleans up StopIconFactory

--- Application/Mapping/StopIconFactory.swift
+++ Application/Mapping/StopIconFactory.swift
@@ -7,47 +7,69 @@
 
 import UIKit
 
-/// Renders a stop icon 'badge'. In other words, the core of a stop annotation view's rendered appearance on the map.
+/// Renders a stop icon. This is the transport type icon, a directional arrow, and all of the other adornments that go with it.
 public class StopIconFactory: NSObject {
     private let kUseDebugColors = false
 
     private let iconSize: CGFloat
 
-    private var iconBackgroundColor: UIColor = .white // abxoxo dark mode
     private var chevronFillColor: UIColor = .red // abxoxo dark mode
-    private var textColor: UIColor = .white
     private let iconCache = NSCache<NSString, UIImage>()
 
+    /// Initializes a `StopIconFactory`.
+    /// - Parameter iconSize: The width and height of the icons that will be generated.
     public init(iconSize: CGFloat) {
         self.iconSize = iconSize
     }
 
+    /// Creates a stop icon image.
+    /// - Parameter stop: The `Stop` for which an image will be rendered.
+    /// - Parameter strokeColor: The border color of the icon.
+    /// - Parameter fillColor: The interior color of the icon.
     public func buildIcon(for stop: Stop, strokeColor: UIColor, fillColor: UIColor) -> UIImage {
         // First, let's compose the cache key out of the name and orientation, then
         // see if we've already got one that matches.
-
         let key = cacheKey(for: stop, strokeColor: strokeColor, fillColor: fillColor) as NSString
 
+        // If an image already exists, then go ahead and return it.
         if let cachedImage = iconCache.object(forKey: key) {
             return cachedImage
         }
 
+        // Otherwise, generate a new stop icon and add it to the cache.
         let image = renderImage(for: stop, strokeColor: strokeColor, fillColor: fillColor)
         iconCache.setObject(image, forKey: key)
 
         return image
     }
 
+    /// The outer gutter for the directional arrow.
     private let arrowTrackSize: CGFloat = 8.0
+
+    /// The size of the directional arrow.
     private let arrowSize = CGSize(width: 12, height: 6)
+
+    /// The line width of the arrow's border.
     private let arrowStroke: CGFloat = 1.0
+
+    /// The corner radius of the icon.
     private let cornerRadius: CGFloat = 4.0
+
+    /// The stroke width.
     private let borderWidth: CGFloat = 2.0
+
+    /// The opacity of the icon's background.
     private let backgroundAlpha: CGFloat = 0.9
-    private let iconInset: CGFloat = 6.0
+
+    /// The drawing inset of the transport glyph.
+    private let transportGlyphInset: CGFloat = 6.0
 
     // MARK: - Rendering Steps
 
+    /// Draws the stop icon image.
+    /// - Parameter stop: The `Stop` for which an icon will be generated.
+    /// - Parameter strokeColor: The border color.
+    /// - Parameter fillColor: The interior color.
     private func renderImage(for stop: Stop, strokeColor: UIColor, fillColor: UIColor) -> UIImage {
         let imageBounds = CGRect(x: 0, y: 0, width: iconSize, height: iconSize)
         let rect = imageBounds.insetBy(dx: arrowTrackSize, dy: arrowTrackSize)
@@ -63,12 +85,16 @@ public class StopIconFactory: NSObject {
             }
 
             self.drawBackground(color: fillColor, rect: rect, context: ctx)
-            self.drawBorder(rect: rect, color: strokeColor, context: ctx)
+            self.drawBorder(color: strokeColor, rect: rect, context: ctx)
             self.drawIcon(routeType: stop.prioritizedRouteTypeForDisplay, rect: rect, context: ctx)
             self.drawArrowImage(direction: stop.direction, strokeColor: strokeColor, rect: imageBounds, context: ctx)
         }
     }
 
+    /// Draws the background color of the icon, accounting for the corner radius.
+    /// - Parameter color: Background color.
+    /// - Parameter rect: The rectangle in which to draw.
+    /// - Parameter context: The Core Graphics context in which drawing happens.
     private func drawBackground(color: UIColor, rect: CGRect, context: CGContext) {
         context.pushPop {
             // we inset the rect the 1/2 px in order ensure that it doesn't 'bleed' past
@@ -79,7 +105,11 @@ public class StopIconFactory: NSObject {
         }
     }
 
-    private func drawBorder(rect: CGRect, color: UIColor, context: CGContext) {
+    /// Strokes the border color onto the icon.
+    /// - Parameter color: Stroke/border color.
+    /// - Parameter rect: The rectangle in which to draw.
+    /// - Parameter context: The Core Graphics context in which drawing happens.
+    private func drawBorder(color: UIColor, rect: CGRect, context: CGContext) {
         context.pushPop {
             let inset = borderWidth / 2.0
             let bezierPath = UIBezierPath(roundedRect: rect.insetBy(dx: inset, dy: inset), cornerRadius: cornerRadius)
@@ -89,13 +119,24 @@ public class StopIconFactory: NSObject {
         }
     }
 
+    /// Draws the transport glyph onto the icon.
+    /// - Note: Drawing is inset from `rect` by `transportGlyphInset` points.
+    ///
+    /// - Parameter routeType: The transport glyph type that will be drawn.
+    /// - Parameter rect: The rectangle in which to draw.
+    /// - Parameter context: The Core Graphics context in which drawing happens.
     private func drawIcon(routeType: RouteType, rect: CGRect, context: CGContext) {
         context.pushPop {
             let image = Icons.transportIcon(from: routeType)
-            image.draw(in: rect.insetBy(dx: iconInset, dy: iconInset))
+            image.draw(in: rect.insetBy(dx: transportGlyphInset, dy: transportGlyphInset))
         }
     }
 
+    /// Draws the directional arrow in the outer track of the image.
+    /// - Parameter direction: The direction in which to draw the arrow.
+    /// - Parameter strokeColor: The border color of the arrow.
+    /// - Parameter rect: The rectangle in which to draw.
+    /// - Parameter context: The Core Graphics context in which drawing happens.
     private func drawArrowImage(direction: Direction, strokeColor: UIColor, rect: CGRect, context: CGContext) { // swiftlint:disable:this function_body_length
         guard direction != .unknown else { return }
 
@@ -175,6 +216,10 @@ public class StopIconFactory: NSObject {
 
     // MARK: - Private Helpers
 
+    /// Calculates a cache key for stop icons.
+    /// - Parameter stop: The `Stop`.
+    /// - Parameter strokeColor: The border color.
+    /// - Parameter fillColor: The fill color.
     private func cacheKey(for stop: Stop, strokeColor: UIColor, fillColor: UIColor) -> String {
         let routeType = stop.prioritizedRouteTypeForDisplay.rawValue
         let direction = stop.direction.rawValue
