2f80f50f35959580e13fce4f035aeefe437d401e
Aaron Brethorst <aaron@brethorsting.com>
2019-09-27 23:11:21 -0700

    Cleans up hierarchy of frameworks a bit

--- TripPlanning/WalkingDirections.swift
+++ TripPlanning/WalkingDirections.swift
@@ -11,11 +11,11 @@ import CoreLocation
 /// Provides facilities for calculating walking directions and travel times.
 @objc(OBAWalkingDirections) public class WalkingDirections: NSObject {
 
-    /// Normal human walking speed is 1.4 meters per second (about 3.1 miles per hour).
+    /// Average human walking speed is 1.4 meters per second (about 3.1 miles per hour).
     private static let walkingVelocity = 1.4
 
-    /// Calculates the travel time in seconds from one location to another, assuming a normal human
-    /// walking speed of 1.4 meters per second (about 3.1 miles per hour).
+    /// Calculates the travel time in seconds from one location to another, assuming
+    /// average human walking speed of 1.4 meters per second (about 3.1 miles per hour).
     /// - Parameter location: Starting location
     /// - Parameter toLocation: Ending location
     public class func travelTime(from location: CLLocation?, to toLocation: CLLocation?) -> TimeInterval? {
