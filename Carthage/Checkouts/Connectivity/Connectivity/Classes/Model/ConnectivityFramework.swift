ce7f218097581b9d1308c2034d16b35094737965
Aaron Brethorst <aaron@brethorsting.com>
2019-12-20 11:08:03 -0800

    Adds Connectivity framework to project
    This will be used to detect network and server reachability

---
+++ Carthage/Checkouts/Connectivity/Connectivity/Classes/Model/ConnectivityFramework.swift
@@ -0,0 +1,14 @@
+//
+//  ConnectivityFramework.swift
+//  Connectivity
+//
+//  Created by Ross Butler on 12/9/18.
+//
+
+import Foundation
+
+// MARK: Type definitions
+@objc public enum ConnectivityFramework: Int {
+    case network
+    case systemConfiguration
+}
