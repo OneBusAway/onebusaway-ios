6410be4acae455532139c7ad3f46ab652fb57080
Aaron Brethorst <aaron@brethorsting.com>
2020-01-20 11:08:26 -0800

    Rebuilds the Bookmarks Controller on IGListKit
    Replaces the AloeStackView-based BookmarksViewController with a new one built on top of IGListKit.
    
    Also includes:
    
    * Rudimentary support for expanding and collapsing bookmark sections.
    * Shows an empty data set view when there are no bookmarks.
    * Fixes issues where bookmarks' departures were rendering in the wrong order.

--- OBAKit/Extensions/ListKitExtensions.swift
+++ OBAKit/Extensions/ListKitExtensions.swift
@@ -19,6 +19,8 @@ extension ListAdapterDataSource where Self: AppContext {
     /// - Returns: The `ListSectionController`
     func defaultSectionController(for object: Any) -> ListSectionController {
         switch object {
+        case is BookmarkSectionData:
+            return BookmarkSectionController(formatters: application.formatters)
         case is MessageSectionData:
             return MessageSectionController()
         case is TableSectionData:
