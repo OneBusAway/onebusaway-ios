f6d618e0247576bc94672ac1aa669e5c3b2dd040
Alan Chu <ualch9@gmail.com>
2020-07-19 22:09:05 -0700

    stashing changes
    XcodeGen#623

---
+++ Intents/IntentHandler.swift
@@ -0,0 +1,19 @@
+//
+//  IntentHandler.swift
+//  Intents
+//
+//  Created by Alan Chu on 7/19/20.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
