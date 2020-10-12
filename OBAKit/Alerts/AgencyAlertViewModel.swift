a7bdf89c78f591f840fa1d92de3c844a9f7184a2
Alan Chu <ualch9@gmail.com>
2020-10-12 12:01:34 -0700

    AgencyAlertsViewController → OBAListView

---
+++ OBAKit/Alerts/AgencyAlertViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  AgencyAlertViewModel.swift
+//  OBAKit
+//
+//  Created by Alan Chu on 10/10/20.
+//
+
+import OBAKitCore
+
+extension AgencyAlert {
+    struct ListViewModel: OBAListViewItem {
+        var id: String
+        var title: String
+        var body: String
+        var localizedURL: URL?
+
+        var subtitle: String { return String(body.prefix(256)) }
+        var onSelectAction: OBAListViewAction<ListViewModel>? = nil
+
+        var contentConfiguration: OBAContentConfiguration {
+            var config = OBAListRowConfiguration(
+                text: title,
+                secondaryText: subtitle,
+                appearance: .subtitle,
+                accessoryType: .disclosureIndicator)
+
+            config.image = UIImage(systemName: "exclamationmark.circle")?.applyingSymbolConfiguration(.init(textStyle: .body))
+            config.textConfig.numberOfLines = 2
+            config.secondaryTextConfig.numberOfLines = 3
+
+            config.textConfig.accessibilityNumberOfLines = 5
+            config.secondaryTextConfig.accessibilityNumberOfLines = 8
+            return config
+        }
+
+        init(_ agencyAlert: AgencyAlert, forLocale locale: Locale, onSelectAction: OBAListViewAction<ListViewModel>? = nil) {
+            self.id = agencyAlert.id
+            self.title = agencyAlert.titleForLocale(locale) ?? ""
+            self.body = agencyAlert.bodyForLocale(locale) ?? ""
+            self.localizedURL = agencyAlert.URLForLocale(locale)
+            self.onSelectAction = onSelectAction
+        }
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(id)
+        }
+
+        static func == (lhs: ListViewModel, rhs: ListViewModel) -> Bool {
+            return lhs.title == rhs.title &&
+                lhs.body == rhs.body &&
+                lhs.localizedURL == rhs.localizedURL
+        }
+    }
+
+    var listViewModel: ListViewModel {
+        return ListViewModel(self, forLocale: Locale.current)
+    }
+}
---
+++ OBAKit/Alerts/AgencyAlertsStore+ListView.swift
@@ -0,0 +1,29 @@
+//
+//  AgencyAlertsStore+ListView.swift
+//  OBAKit
+//
+//  Created by Alan Chu on 10/11/20.
+//
+
+import OBAKitCore
+
+extension AgencyAlertsStore {
+    /// Outputs the alerts in this store into a sorted `[OBAListViewSection]` for use with `OBAListView`
+    ///
+    /// - Parameters:
+    ///   - onSelectAction: The optional closure to perform when the user selects the alert in `OBAListView`.
+    /// - Returns: An array of `[OBAListViewSection]` grouped by Agency name. The sections provided will have a `agency_alert` prefix for its section ID.
+    func listViewSections(onSelectAction: OBAListViewAction<AgencyAlert.ListViewModel>? = nil) -> [OBAListViewSection] {
+        let groupedAlerts = Dictionary(grouping: agencyAlerts, by: { $0.agency?.agency.name ?? "" })
+        return groupedAlerts.map { group -> OBAListViewSection in
+            let viewModels = group.value.map { alert -> AgencyAlert.ListViewModel in
+                var viewModel = alert.listViewModel
+                viewModel.onSelectAction = onSelectAction
+                return viewModel
+            }
+
+            let alerts = Set(viewModels).allObjects // remove duplicates
+            return OBAListViewSection(id: "agency_alerts_\(group.key)", title: group.key, contents: alerts)
+        }.sorted(by: \.id)
+    }
+}
