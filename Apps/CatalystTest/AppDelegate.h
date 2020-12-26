c9a73b02569d16722f670ff97f520cb05d2428c0
Aaron Brethorst <aaron@brethorsting.com>
2020-12-26 09:37:11 -0800

    Simple demo of OBAKit running as a Catalyst app

---
+++ Apps/CatalystTest/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  OBANetworking
+//
+//  Copyright © Open Transit Software Foundation
+//  This source code is licensed under the Apache 2.0 license found in the
+//  LICENSE file in the root directory of this source tree.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
