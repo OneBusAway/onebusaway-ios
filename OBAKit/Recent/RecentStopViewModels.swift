71a25e8ee717ee2e0948eafc10434402aec26547
Alan Chu <ualch9@gmail.com>
2021-03-05 12:17:39 -0800

    MapFloatingPanelController → OBAListView

--- OBAKit/Recent/RecentStopViewModels.swift
+++ OBAKit/Recent/RecentStopViewModels.swift
@@ -7,45 +7,50 @@
 
 import OBAKitCore
 
-extension RecentStopsViewController {
-    struct StopViewModel: OBAListViewItem {
-        let name: String
-        let subtitle: String?
+/// A view model for use with OBAListView for displaying basic stop details.
+///
+/// This model uses a default content configuration, there is no need to register this
+/// item with OBAListView before use.
+struct StopViewModel: OBAListViewItem {
+    let name: String
+    let subtitle: String?
 
-        let stopID: String
+    let stopID: Stop.ID
 
-        var contentConfiguration: OBAContentConfiguration {
-            return OBAListRowConfiguration(
-                text: .string(name),
-                secondaryText: .string(subtitle),
-                appearance: .subtitle,
-                accessoryType: .disclosureIndicator)
-        }
-
-        let onSelectAction: OBAListViewAction<StopViewModel>?
-        let onDeleteAction: OBAListViewAction<StopViewModel>?
-
-        init(withStop stop: Stop,
-             onSelect selectAction: OBAListViewAction<StopViewModel>?,
-             onDelete deleteAction: OBAListViewAction<StopViewModel>?) {
-            self.name = stop.name
-            self.subtitle = stop.subtitle
-
-            self.stopID = stop.id
-            self.onSelectAction = selectAction
-            self.onDeleteAction = deleteAction
-        }
-
-        func hash(into hasher: inout Hasher) {
-            hasher.combine(stopID)
-        }
-
-        static func == (lhs: StopViewModel, rhs: StopViewModel) -> Bool {
-            return lhs.name == rhs.name &&
-                lhs.subtitle == rhs.subtitle
-        }
+    var contentConfiguration: OBAContentConfiguration {
+        return OBAListRowConfiguration(
+            text: .string(name),
+            secondaryText: .string(subtitle),
+            appearance: .subtitle,
+            accessoryType: .disclosureIndicator)
     }
 
+    let onSelectAction: OBAListViewAction<StopViewModel>?
+    let onDeleteAction: OBAListViewAction<StopViewModel>?
+
+    init(withStop stop: Stop,
+         onSelect selectAction: OBAListViewAction<StopViewModel>?,
+         onDelete deleteAction: OBAListViewAction<StopViewModel>?) {
+        self.name = stop.name
+        self.subtitle = stop.subtitle
+
+        self.stopID = stop.id
+        self.onSelectAction = selectAction
+        self.onDeleteAction = deleteAction
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(stopID)
+    }
+
+    static func == (lhs: StopViewModel, rhs: StopViewModel) -> Bool {
+        return lhs.stopID == rhs.stopID &&
+            lhs.name == rhs.name &&
+            lhs.subtitle == rhs.subtitle
+    }
+}
+
+extension RecentStopsViewController {
     struct AlarmViewModel: OBAListViewItem {
         let alarm: Alarm
         let deepLink: ArrivalDepartureDeepLink
