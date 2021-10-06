d2cfb2bc5d6c62ca436c8c7a5162e061cefde1a8
Alan Chu <ualch9@gmail.com>
2021-10-06 13:04:02 -0700

    Add StopBookmarkView and model

---
+++ OBAKit/Bookmarks/StopBookmarkView.swift
@@ -0,0 +1,28 @@
+//
+//  StopBookmarkView.swift
+//  OBAKit
+//
+//  Created by Alan Chu on 10/6/21.
+//
+
+import SwiftUI
+
+struct StopBookmarkView: View {
+    @State var viewModel: StopBookmarkViewModel
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            RouteTypeLabelView(labelText: "Stop #\(viewModel.stopID)", routeType: viewModel.primaryRouteType)
+            Text(viewModel.name)
+        }
+    }
+}
+
+struct StopBookmarkView_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            StopBookmarkView(viewModel: .soundTransitUDistrict)
+            StopBookmarkView(viewModel: .ferrySeattle)
+        }
+    }
+}
---
+++ OBAKit/Bookmarks/StopBookmarkViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  StopBookmarkViewModel.swift
+//  OBAKit
+//
+//  Created by Alan Chu on 10/6/21.
+//
+
+import Foundation
+import OBAKitCore
+
+struct StopBookmarkViewModel: Identifiable {
+    let id: String
+
+    let name: String
+    let stopID: StopID
+    let primaryRouteType: Route.RouteType
+
+    let isFavorite: Bool
+}
+
+#if DEBUG
+extension StopBookmarkViewModel {
+    static func preview(
+        name: String,
+        stopID: StopID,
+        primaryRouteType: Route.RouteType,
+        isFavorite: Bool
+    ) -> Self {
+        return self.init(
+            id: UUID().uuidString,
+            name: name,
+            stopID: stopID,
+            primaryRouteType: primaryRouteType,
+            isFavorite: isFavorite)
+    }
+
+    static var soundTransitUDistrict: Self {
+        return .preview(name: "U District", stopID: "990002", primaryRouteType: .lightRail, isFavorite: true)
+    }
+
+    static var ferrySeattle: Self {
+        return .preview(name: "WSF Seattle", stopID: "7", primaryRouteType: .ferry, isFavorite: false)
+    }
+}
+#endif
