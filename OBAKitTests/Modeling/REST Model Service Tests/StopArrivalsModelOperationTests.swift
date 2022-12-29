59a97319ab2ad7a60fc60e5ea792c56b68c4d54b
Alan Chu <ualch9@gmail.com>
2022-12-29 15:08:20 -0800

    update stoparrivals test

--- OBAKitTests/Modeling/REST Model Service Tests/StopArrivalsModelOperationTests.swift
+++ OBAKitTests/Modeling/REST Model Service Tests/StopArrivalsModelOperationTests.swift
@@ -28,7 +28,7 @@ class StopArrivalsModelOperationTests: OBATestCase {
     override func setUp() {
         super.setUp()
 
-        let dataLoader = (restService.dataLoader as! MockDataLoader)
+        let dataLoader = (betterRESTService.dataLoader as! MockDataLoader)
 
         dataLoader.mock(
             URLString: makeUrlString(stopID: campusParkwayStopID),
@@ -46,136 +46,101 @@ class StopArrivalsModelOperationTests: OBATestCase {
         )
     }
 
-    func test_arrivalAndDepartureStatus() {
-        let op = restService.getArrivalsAndDeparturesForStop(id: galerStopID, minutesBefore: 5, minutesAfter: 30)
-        waitUntil { done in
-            op.complete { result in
-                switch result {
-                case .failure:
-                    fatalError()
-                case .success(let response):
-                    let arrivals = response.entry
-                    expect(arrivals.arrivalsAndDepartures.count) == 5
+    func test_arrivalAndDepartureStatus() async throws {
+        let arrivals = try await betterRESTService.getArrivalsAndDeparturesForStop(id: galerStopID, minutesBefore: 5, minutesAfter: 30).entry
 
-                    expect(arrivals.arrivalsAndDepartures[0].arrivalDepartureStatus) == .arriving
-                    expect(arrivals.arrivalsAndDepartures[1].arrivalDepartureStatus) == .departing
+        expect(arrivals.arrivalsAndDepartures.count) == 5
 
-                    expect(arrivals.arrivalsAndDepartures[0].vehicleID) == "1_4361"
-                    expect(arrivals.arrivalsAndDepartures[1].vehicleID) == "1_4361"
+        expect(arrivals.arrivalsAndDepartures[0].arrivalDepartureStatus) == .arriving
+        expect(arrivals.arrivalsAndDepartures[1].arrivalDepartureStatus) == .departing
 
-                    expect(arrivals.arrivalsAndDepartures[2].arrivalDepartureStatus) == .arriving
-                    expect(arrivals.arrivalsAndDepartures[3].arrivalDepartureStatus) == .departing
-                    expect(arrivals.arrivalsAndDepartures[4].arrivalDepartureStatus) == .arriving
+        expect(arrivals.arrivalsAndDepartures[0].vehicleID) == "1_4361"
+        expect(arrivals.arrivalsAndDepartures[1].vehicleID) == "1_4361"
 
-                    done()
-                }
-            }
-        }
+        expect(arrivals.arrivalsAndDepartures[2].arrivalDepartureStatus) == .arriving
+        expect(arrivals.arrivalsAndDepartures[3].arrivalDepartureStatus) == .departing
+        expect(arrivals.arrivalsAndDepartures[4].arrivalDepartureStatus) == .arriving
     }
 
-    func testLoading_success() {
-        let op = restService.getArrivalsAndDeparturesForStop(id: campusParkwayStopID, minutesBefore: 5, minutesAfter: 30)
+    func testLoading_success() async throws {
+        let arrivals = try await betterRESTService.getArrivalsAndDeparturesForStop(id: campusParkwayStopID, minutesBefore: 5, minutesAfter: 30).entry
 
-        waitUntil { (done) in
-            op.complete { result in
-                switch result {
-                case .failure:
-                    fatalError()
-                case .success(let response):
-                    let arrivals = response.entry
-                    expect(arrivals.nearbyStops.count) == 4
-                    expect(arrivals.nearbyStops.count) == 4
-                    expect(arrivals.nearbyStops.first!.name) == "15th Ave NE & NE Campus Pkwy"
+        expect(arrivals.nearbyStops.count) == 4
+        expect(arrivals.nearbyStops.count) == 4
+        expect(try XCTUnwrap(arrivals.nearbyStops.first).name) == "15th Ave NE & NE Campus Pkwy"
 
-                    expect(arrivals.serviceAlerts.count) == 0
+        expect(arrivals.serviceAlerts.count) == 0
 
-                    expect(arrivals.stop.id) == "1_10914"
-                    expect(arrivals.stop.name) == "15th Ave NE & NE Campus Pkwy"
+        expect(arrivals.stop.id) == "1_10914"
+        expect(arrivals.stop.name) == "15th Ave NE & NE Campus Pkwy"
 
-                    expect(arrivals.arrivalsAndDepartures.count) == 1
+        expect(arrivals.arrivalsAndDepartures.count) == 1
 
-                    let arrDep = arrivals.arrivalsAndDepartures.first!
-                    expect(arrDep.arrivalEnabled).to(beTrue())
-                    expect(arrDep.blockTripSequence) == 9
-                    expect(arrDep.departureEnabled).to(beTrue())
-                    expect(arrDep.distanceFromStop).to(beCloseTo(1232.648659247323))
-                    expect(arrDep.frequency).to(beNil())
+        let arrDep = try XCTUnwrap(arrivals.arrivalsAndDepartures.first)
+        expect(arrDep.arrivalEnabled).to(beTrue())
+        expect(arrDep.blockTripSequence) == 9
+        expect(arrDep.departureEnabled).to(beTrue())
+        expect(arrDep.distanceFromStop).to(beCloseTo(1232.648659247323))
+        expect(arrDep.frequency).to(beNil())
 
-                    expect(arrDep.lastUpdated) == Date.fromComponents(year: 2018, month: 11, day: 02, hour: 06, minute: 55, second: 49)
+        expect(arrDep.lastUpdated) == Date.fromComponents(year: 2018, month: 11, day: 02, hour: 06, minute: 55, second: 49)
 
-                    expect(arrDep.numberOfStopsAway) == 4
-                    expect(arrDep.predicted).to(beTrue())
+        expect(arrDep.numberOfStopsAway) == 4
+        expect(arrDep.predicted).to(beTrue())
 
-                    expect(arrDep.arrivalDepartureDate) == Date.fromComponents(year: 2018, month: 11, day: 02, hour: 07, minute: 02, second: 36)
+        expect(arrDep.arrivalDepartureDate) == Date.fromComponents(year: 2018, month: 11, day: 02, hour: 07, minute: 02, second: 36)
 
-                    expect(arrDep.route.id) == "1_100447"
-                    expect(arrDep.route.shortName) == "49"
+        expect(arrDep.route.id) == "1_100447"
+        expect(arrDep.route.shortName) == "49"
 
-                    expect(arrDep.routeLongName).to(beNil())
-                    expect(arrDep.routeShortName) == "49"
+        expect(arrDep.routeLongName).to(beNil())
+        expect(arrDep.routeShortName) == "49"
 
-                    expect(arrDep.arrivalDepartureDate) == Date.fromComponents(year: 2018, month: 11, day: 02, hour: 07, minute: 02, second: 36)
+        expect(arrDep.arrivalDepartureDate) == Date.fromComponents(year: 2018, month: 11, day: 02, hour: 07, minute: 02, second: 36)
 
-                    expect(arrDep.serviceDate) == Date.fromComponents(year: 2018, month: 11, day: 01, hour: 07, minute: 00, second: 00)
+        expect(arrDep.serviceDate) == Date.fromComponents(year: 2018, month: 11, day: 01, hour: 07, minute: 00, second: 00)
 
-                    expect(arrDep.serviceAlerts.count) == 0
+        expect(arrDep.serviceAlerts.count) == 0
 
-                    expect(arrDep.status) == "default"
+        expect(arrDep.status) == "default"
 
-                    expect(arrDep.stop.id) == "1_10914"
-                    expect(arrDep.stop.name) == "15th Ave NE & NE Campus Pkwy"
+        expect(arrDep.stop.id) == "1_10914"
+        expect(arrDep.stop.name) == "15th Ave NE & NE Campus Pkwy"
 
-                    expect(arrDep.stopSequence) == 3
+        expect(arrDep.stopSequence) == 3
 
-                    expect(arrDep.totalStopsInTrip) == 22
+        expect(arrDep.totalStopsInTrip) == 22
 
-                    expect(arrDep.tripHeadsign) == "Downtown Seattle Broadway"
+        expect(arrDep.tripHeadsign) == "Downtown Seattle Broadway"
 
-                    expect(arrDep.trip.id) == "1_40984902"
-                    expect(arrDep.trip.shortName) == "LOCAL"
+        expect(arrDep.trip.id) == "1_40984902"
+        expect(arrDep.trip.shortName) == "LOCAL"
 
-                    expect(arrDep.tripStatus).toNot(beNil())
-                    let tripStatus = arrDep.tripStatus!
-                    expect(tripStatus.activeTrip.id) == "1_40984840"
+        expect(arrDep.tripStatus).toNot(beNil())
+        let tripStatus = try XCTUnwrap(arrDep.tripStatus)
+        expect(tripStatus.activeTrip.id) == "1_40984840"
 
-                    expect(arrDep.vehicleID) == "1_4559"
-
-                    done()
-                }
-            }
-        }
+        expect(arrDep.vehicleID) == "1_4559"
     }
 
-    func testLoading_rogueValley() {
+    func testLoading_rogueValley() async throws {
         // There are some indications that the data shape from RVTD is different from some other regions.
         // This test is meant to ensure that these different data sources work equally well.
 
-        let op = restService.getArrivalsAndDeparturesForStop(id: rvtdStopID, minutesBefore: 5, minutesAfter: 30)
-        waitUntil { (done) in
-            op.complete { result in
-                switch result {
-                case .failure:
-                    fatalError()
-                case .success(let response):
-                    let arrivals = response.entry
-                    expect(arrivals.nearbyStops.count) == 3
-                    expect(arrivals.serviceAlerts.count) == 0
+        let arrivals = try await betterRESTService.getArrivalsAndDeparturesForStop(id: rvtdStopID, minutesBefore: 5, minutesAfter: 30).entry
+        expect(arrivals.nearbyStops.count) == 3
+        expect(arrivals.serviceAlerts.count) == 0
 
-                    expect(arrivals.stop.id) == "1739_d1e8e68e-83f8-487f-baf5-f465fe70fc84"
-                    expect(arrivals.stop.name) == "E Main St - West of Myrtle St"
+        expect(arrivals.stop.id) == "1739_d1e8e68e-83f8-487f-baf5-f465fe70fc84"
+        expect(arrivals.stop.name) == "E Main St - West of Myrtle St"
 
-                    expect(arrivals.arrivalsAndDepartures.count) == 1
+        expect(arrivals.arrivalsAndDepartures.count) == 1
 
-                    let arrDep = arrivals.arrivalsAndDepartures.first!
-                    expect(arrDep.arrivalEnabled).to(beTrue())
-                    expect(arrDep.blockTripSequence) == 2
-                    expect(arrDep.departureEnabled).to(beTrue())
-                    expect(arrDep.distanceFromStop).to(beCloseTo(63293.0860))
-                    expect(arrDep.frequency).to(beNil())
-
-                    done()
-                }
-            }
-        }
+        let arrDep = try XCTUnwrap(arrivals.arrivalsAndDepartures.first)
+        expect(arrDep.arrivalEnabled).to(beTrue())
+        expect(arrDep.blockTripSequence) == 2
+        expect(arrDep.departureEnabled).to(beTrue())
+        expect(arrDep.distanceFromStop).to(beCloseTo(63293.0860))
+        expect(arrDep.frequency).to(beNil())
     }
 }
