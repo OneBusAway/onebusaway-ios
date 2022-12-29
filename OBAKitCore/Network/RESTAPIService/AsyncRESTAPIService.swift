c00a5a98ebda155457ba8387f15d3d94e50499dc
Alan Chu <ualch9@gmail.com>
2022-12-29 10:34:55 -0800

    delete dead code

--- OBAKitCore/Network/RESTAPIService/AsyncRESTAPIService.swift
+++ OBAKitCore/Network/RESTAPIService/AsyncRESTAPIService.swift
@@ -70,44 +70,4 @@ public actor RESTAPIService {
         let urlString: String = response?.url?.absoluteString ?? "(unknown url)"
         logger.error("\(urlString): \(description)")
     }
-
-    // MARK: - URL Builders -
-
-    nonisolated func generateURL(path: String, params: [String: Any]? = nil) -> URL {
-        let urlString = joinBaseURLToPath(path)
-        let queryParamString = buildQueryParams(params)
-        let fullURLString = String(format: "%@?%@", urlString, queryParamString)
-
-        return URL(string: fullURLString)!
-    }
-
-    private nonisolated func joinBaseURLToPath(_ path: String) -> String {
-        let baseURLString = configuration.baseURL.absoluteString
-
-        if baseURLString.hasSuffix("/") && path.hasPrefix("/") {
-            return baseURLString + String(path.dropFirst())
-        }
-        else if !baseURLString.hasSuffix("/") && !path.hasPrefix("/") {
-            return String(format: "%@/%@", baseURLString, path)
-        }
-        else {
-            return baseURLString + path
-        }
-    }
-
-    /// Takes in a hash of params and this object's default query items, and produces a list of `&`-separated `key=value` pairs.
-    /// - Parameter params: Additional query parameters having to do with the in-flight API call.
-    private nonisolated func buildQueryParams(_ params: [String: Any]? = nil) -> String {
-        let allQueryItems: [URLQueryItem] = NetworkHelpers.dictionary(toQueryItems: params ?? [:]) + configuration.defaultQueryItems
-        return allQueryItems.compactMap { queryItem in
-            guard
-                let key = queryItem.name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-                let value = queryItem.value?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-            else {
-                return nil
-            }
-
-            return String(format: "%@=%@", key, value)
-        }.joined(separator: "&")
-    }
 }
