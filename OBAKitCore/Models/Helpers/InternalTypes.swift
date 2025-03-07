dac29890e93f088f8130c829260cadd97b9a1d35
Aditya Anand <anandaditya.ind@gmail.com>
2025-03-08 00:38:36 +0530

    Fix the JSON decoder extension for Weather Button

--- OBAKitCore/Models/Helpers/InternalTypes.swift
+++ OBAKitCore/Models/Helpers/InternalTypes.swift
@@ -36,11 +36,23 @@ extension CLLocation {
 
 extension JSONDecoder {
     class var obacoServiceDecoder: JSONDecoder {
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .iso8601
+            let decoder = JSONDecoder()
+            let formatter = ISO8601DateFormatter()
+            formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds, .withTimeZone] // Handles the milliseconds
+            decoder.dateDecodingStrategy = .custom { decoder in
+                let container = try decoder.singleValueContainer()
+                let dateString = try container.decode(String.self)
+                let fixedDateString = dateString.replacingOccurrences(of: "+00:00", with: "Z")
 
-        return decoder
-    }
+                if let date = formatter.date(from: fixedDateString) {
+                    return date
+                } else {
+                    throw DecodingError.dataCorruptedError(in: container, debugDescription: "Invalid date format")
+                }
+            }
+
+            return decoder
+        }
 
     class func RESTDecoder(regionIdentifier: Int? = nil) -> JSONDecoder {
         let decoder = JSONDecoder()
