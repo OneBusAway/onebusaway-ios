bdb09cce456420ae2aba1aae31f2714be24cc4ef
Manu R <manurajbhar12@gmail.com>
2025-08-28 22:36:10 +0530

    Integrate OTPKit into MapViewController

--- OBAKit/Bookmarks/BookmarksViewController.swift
+++ OBAKit/Bookmarks/BookmarksViewController.swift
@@ -8,24 +8,21 @@
 //
 
 import UIKit
-import SwiftUI
 import CoreLocation
 import OBAKitCore
 import WidgetKit
-import OTPKit
-import MapKit
 
 /// The view controller that powers the Bookmarks tab of the app.
 @objc(OBABookmarksViewController)
 public class BookmarksViewController: UIViewController,
-    AppContext,
-    BookmarkEditorDelegate,
-    BookmarkDataDelegate,
-    ManageBookmarksDelegate,
-    ModalDelegate,
-    OBAListViewDataSource,
-    OBAListViewCollapsibleSectionsDelegate,
-    OBAListViewContextMenuDelegate {
+                                      AppContext,
+                                      BookmarkEditorDelegate,
+                                      BookmarkDataDelegate,
+                                      ManageBookmarksDelegate,
+                                      ModalDelegate,
+                                      OBAListViewDataSource,
+                                      OBAListViewCollapsibleSectionsDelegate,
+                                      OBAListViewContextMenuDelegate {
 
     let application: Application
 
@@ -158,13 +155,8 @@ public class BookmarksViewController: UIViewController,
             distanceSortAction.state = .on
         }
 
-        let otpAction = UIAction(title: "Trip Planner", image: UIImage(systemName: "map.fill")) { _ in
-            self.openOTPView()
-        }
-        
         let sortMenu = UIMenu(title: Strings.sort, options: .displayInline, children: [groupSortAction, distanceSortAction])
-        let mainMenu = UIMenu(children: [otpAction, sortMenu])
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "MORE", image: UIImage(systemName: "ellipsis.circle"), menu: mainMenu)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "MORE", image: UIImage(systemName: "arrow.up.arrow.down.circle"), menu: sortMenu)
     }
 
     // MARK: Refresh Widget
@@ -238,17 +230,17 @@ public class BookmarksViewController: UIViewController,
         let body: String
 
         if application.hasDataToMigrate {
-                title = Strings.emptyBookmarkTitle
-                body = Strings.emptyBookmarkBodyWithPendingMigration
-            }
-            else if application.userDataStore.bookmarks.isEmpty {
-                title = Strings.emptyBookmarkTitle
-                body = Strings.emptyBookmarkBody
-            }
-            else {
-                // Don't show empty state if we have bookmarks
-                return nil
-            }
+            title = Strings.emptyBookmarkTitle
+            body = Strings.emptyBookmarkBodyWithPendingMigration
+        }
+        else if application.userDataStore.bookmarks.isEmpty {
+            title = Strings.emptyBookmarkTitle
+            body = Strings.emptyBookmarkBody
+        }
+        else {
+            // Don't show empty state if we have bookmarks
+            return nil
+        }
 
         return .standard(.init(title: title, body: body))
     }
@@ -295,62 +287,10 @@ public class BookmarksViewController: UIViewController,
         })
 
         return [buildListSection(
-                    bookmarks: bookmarks,
-                    id: "distance_sorted_group",
-                    title: OBALoc("bookmarks_controller.sorted_by_distance_header", value: "Sorted by Distance", comment: "The table section header on the bookmarks controller for when bookmarks are sorted by distance.")
-            )].compactMap({$0})
-    }
-
-    // MARK: - OTP Actions
-    @objc private func openOTPView() {
-        // Create OTP configuration with a demo server URL and region based on current region
-        let serverURL = URL(string: "https://otp.prod.sound.obaweb.org/otp/routers/default")!
-        let region: MapCameraPosition
-
-        if let currentRegion = application.regionsService.currentRegion {
-            let center = CLLocationCoordinate2D(
-                latitude: currentRegion.centerCoordinate.latitude,
-                longitude: currentRegion.centerCoordinate.longitude
-            )
-            region = .region(MKCoordinateRegion(
-                center: center,
-                span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-            ))
-        } else {
-            // Default to Seattle region
-            region = .region(MKCoordinateRegion(
-                center: CLLocationCoordinate2D(latitude: 47.6062, longitude: -122.3321),
-                span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-            ))
-        }
-
-        let config = OTPConfiguration(
-            otpServerURL: serverURL,
-            themeConfiguration: .init(
-                primaryColor: Color(uiColor: ThemeColors().brand)
-            ),
-            region: region
-        )
-
-        let apiService = RestAPIService(baseURL: serverURL)
-
-        // Get current location for origin
-        var origin: Location?
-        if let currentLocation = application.locationService.currentLocation {
-            origin = Location(
-                title: "OBA Current Location",
-                subTitle: "Your current location",
-                latitude: currentLocation.coordinate.latitude,
-                longitude: currentLocation.coordinate.longitude
-            )
-        }
-
-        let otpView = OTPView(otpConfig: config, apiService: apiService, origin: origin)
-
-        let hostingController = UIHostingController(rootView: otpView)
-        hostingController.modalPresentationStyle = .overFullScreen
-
-        present(hostingController, animated: true)
+            bookmarks: bookmarks,
+            id: "distance_sorted_group",
+            title: OBALoc("bookmarks_controller.sorted_by_distance_header", value: "Sorted by Distance", comment: "The table section header on the bookmarks controller for when bookmarks are sorted by distance.")
+        )].compactMap({$0})
     }
 
     // MARK: - Bookmark Actions
@@ -412,8 +352,8 @@ public class BookmarksViewController: UIViewController,
         }
 
         return OBAListViewMenuActions(previewProvider: previewProvider,
-                               performPreviewAction: commitPreviewAction,
-                               contextMenuProvider: menu)
+                                      performPreviewAction: commitPreviewAction,
+                                      contextMenuProvider: menu)
     }
 
     // MARK: - Arrival departure highlight updates
