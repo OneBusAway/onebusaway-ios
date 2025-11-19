00d23610bf5519f20a391a8f83368455ae1d6d8d
mosliem <mohamedmostafa191299@gmail.com>
2025-11-20 01:45:23 +0200

    Implement survey submission request/response models

---
+++ OBAKitCore/Models/REST/Surveys/SurveySubmission.swift
@@ -0,0 +1,59 @@
+//
+//  SurveySubmission.swift
+//  OBAKitCore
+//
+//  Created by Mohamed Sliem on 20/11/2025.
+//
+
+import Foundation
+
+
+public struct SurveySubmission: Codable {
+    
+    public let userIdentifier: String
+    
+    public let surveyId: Int
+    
+    public let stopIdentifier: String?
+    
+    public let stopLongitude: Double?
+    
+    public let stopLatitude: Double?
+    
+    public let response: [QuestionAnswerSubmission]
+    
+}
+
+public struct QuestionAnswerSubmission: Codable {
+    
+    public let questionId: String
+    
+    public let questionType: String
+    
+    public let questionLabel: String
+    
+    public let answer: String
+    
+    enum CodingKeys: String, CodingKey {
+        case questionId = "question_id"
+        case questionType = "question_type"
+        case questionLabel = "question_label"
+        case answer
+    }
+}
+
+public struct SurveySubmissionResponse: Codable {
+    
+    public let id: String
+    
+    public let updatePath: String
+    
+    public let userIdentifier: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case updatePath = "update_path"
+        case userIdentifier = "user_identifier"
+    }
+    
+}
