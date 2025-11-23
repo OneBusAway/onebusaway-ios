4b3a0c0169f08b083a09ded61a45f3986eb3fff5
mosliem <mohamedmostafa191299@gmail.com>
2025-11-24 01:15:45 +0200

    Update: Files header block

--- OBAKitCore/Network/APIService/HTTPMethod.swift
+++ OBAKitCore/Network/APIService/HTTPMethod.swift
@@ -2,7 +2,9 @@
 //  HTTPMethod.swift
 //  OBAKitCore
 //
-//  Created by Mohamed Sliem on 23/11/2025.
+//  Copyright © Open Transit Software Foundation
+//  This source code is licensed under the Apache 2.0 license found in the
+//  LICENSE file in the root directory of this source tree.
 //
 
 import Foundation
--- OBAKitCore/Network/SurveyAPIService.swift
+++ OBAKitCore/Network/SurveyAPIService.swift
@@ -2,7 +2,9 @@
 //  SurveyAPIService.swift
 //  OBAKitCore
 //
-//  Created by Mohamed Sliem on 22/11/2025.
+//  Copyright © Open Transit Software Foundation
+//  This source code is licensed under the Apache 2.0 license found in the
+//  LICENSE file in the root directory of this source tree.
 //
 
 import Foundation
