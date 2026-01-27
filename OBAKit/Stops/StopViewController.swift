fb1359d79649235003b2de2b68fbbb526fad9707
mosliem <mohamedmostafa191299@gmail.com>
2026-01-27 02:12:57 +0200

    Fix: [SurveyFullQuestions] Update question error state method.

--- OBAKit/Stops/StopViewController.swift
+++ OBAKit/Stops/StopViewController.swift
@@ -1516,7 +1516,7 @@ public class StopViewController: UIViewController,
         withObservationTracking { [weak self] in
             guard let self else { return }
             if self.surveysVM.showSurveyDismissSheet {
-                self.showSurveyDismissActionSheet(self)
+                self.showSurveyDismissActionSheet()
             }
         } onChange: {
             Task { @MainActor [weak self] in
