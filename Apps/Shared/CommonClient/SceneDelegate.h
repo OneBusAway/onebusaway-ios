3822a06d73e822b777817be2da7f872cbd739756
Aaron Brethorst <aaron@brethorsting.com>
2026-06-09 23:15:49 -0700

    Finally embraces UIScene since it is now required on iOS 27

---
+++ Apps/Shared/CommonClient/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  OBANetworking
+//
+//  Copyright © Open Transit Software Foundation
+//  This source code is licensed under the Apache 2.0 license found in the
+//  LICENSE file in the root directory of this source tree.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
